940341a30b9b5132dc1c6755f42b1ab2d6aad719
CockroachSoulful <cockroach888@outlook.com>
2021-02-19 23:32:52 +0800

    feat: 完成DBUtility.Oracle类库迁移。

---
+++ src/ToolKits.DBUtility/ToolKits.DBUtility.Oracle/src/ConnectionString.cs
@@ -0,0 +1,51 @@
+﻿//=========================================================================
+//**   魂哥常用工具集（GSA.MOLLE.ToolKits）
+//=========================================================================
+//**   脉脉含情的充满精神的高尚的小强精神
+//**   风幽思静繁花落；夜半楼台听江雨。（cockroach888@outlook.com）
+//=========================================================================
+//**   Copyright © 蟑螂·魂 2014 -- Support 文雀
+//=========================================================================
+// 文件名称：ConnectionString.cs
+// 项目名称：数据库操作实用工具集
+// 创建时间：2014年2月26日15时31分
+// 创建人员：宋杰军
+// 负责人员：宋杰军
+// 参与人员：宋杰军
+// ========================================================================
+// 修改日期：
+// 修改人员：
+// 修改内容：
+// ========================================================================
+using System;
+using System.Configuration;
+
+namespace GSA.ToolKits.DBUtility
+{
+    /// <summary>
+    /// 数据库连接字符串
+    /// <para>Data Source=MyOracleDB;User Id=myUsername;Password=myPassword;Integrated Security=no;</para>
+    /// </summary>
+    public sealed class OracleConnectionString
+    {
+        /// <summary>
+        /// 数据库连接字符串
+        /// <para>默认名称：OracleConnectionString</para>
+        /// </summary>
+        /// <param name="KeyName">键值名称</param>
+        /// <returns>数据库连接字符串</returns>
+        public static string ConnectionString(string KeyName)
+        {
+            string result = null;
+            if (!string.IsNullOrEmpty(KeyName))
+            {
+                result = ConfigurationManager.ConnectionStrings[KeyName].ConnectionString;
+            }
+            else
+            {
+                result = ConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString;
+            }
+            return result;
+        }
+    }
+}
---
+++ src/ToolKits.DBUtility/ToolKits.DBUtility.Oracle/src/OracleHelperParameterCache.cs
@@ -0,0 +1,154 @@
+﻿//=========================================================================
+//**   魂哥常用工具集（GSA.MOLLE.ToolKits）
+//=========================================================================
+//**   脉脉含情的充满精神的高尚的小强精神
+//**   风幽思静繁花落；夜半楼台听江雨。（cockroach888@outlook.com）
+//=========================================================================
+//**   Copyright © 蟑螂·魂 2014 -- Support 文雀
+//=========================================================================
+// 文件名称：OracleHelperParameterCache.cs
+// 项目名称：数据库操作实用工具集
+// 创建时间：2014年2月26日15时3分
+// 创建人员：宋杰军
+// 负责人员：宋杰军
+// 参与人员：宋杰军
+// ========================================================================
+// 修改日期：
+// 修改人员：
+// 修改内容：
+// ========================================================================
+using System;
+using System.Collections;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace GSA.ToolKits.DBUtility
+{
+    /// <summary>
+    /// OracleHelperParameterCache provides functions to leverage a static cache of procedure parameters, and the
+    /// ability to discover parameters for stored procedures at run-time.
+    /// </summary>
+    public sealed class OracleHelperParameterCache
+    {
+
+        #region private methods, variables, and constructors
+
+        //Since this class provides only static methods, make the default constructor private to prevent 
+        //instances from being created with "new OracleHelperParameterCache()".
+        private OracleHelperParameterCache() { }
+
+        private static Hashtable paramCache = Hashtable.Synchronized(new Hashtable());
+
+        /// <summary>
+        /// resolve at run-time the appropriate set of OracleParameters for a stored procedure
+        /// </summary>
+        /// <param name="connectionString">a valid connection string for an OracleConnection</param>
+        /// <param name="spName">the name of the stored procedure</param>
+        /// <param name="includeReturnValueParameter">whether or not to include ther return value parameter</param>
+        /// <returns></returns>
+        private static OracleParameter[] DiscoverSpParameterSet(string connectionString, string spName, bool includeReturnValueParameter)
+        {
+            using (OracleConnection cn = new OracleConnection(connectionString))
+            using (OracleCommand cmd = new OracleCommand(spName, cn))
+            {
+                cn.Open();
+                cmd.CommandType = CommandType.StoredProcedure;
+                OracleCommandBuilder.DeriveParameters(cmd);
+                if (!includeReturnValueParameter)
+                {
+                    if (ParameterDirection.ReturnValue == cmd.Parameters[0].Direction) cmd.Parameters.RemoveAt(0);
+                }
+                OracleParameter[] discoveredParameters = new OracleParameter[cmd.Parameters.Count];
+                cmd.Parameters.CopyTo(discoveredParameters, 0);
+                return discoveredParameters;
+            }
+        }
+        //deep copy of cached OracleParameter array
+        private static OracleParameter[] CloneParameters(OracleParameter[] originalParameters)
+        {
+            OracleParameter[] clonedParameters = new OracleParameter[originalParameters.Length];
+            for (int i = 0, j = originalParameters.Length; i < j; i++)
+            {
+                clonedParameters[i] = (OracleParameter)((ICloneable)originalParameters[i]).Clone();
+            }
+            return clonedParameters;
+        }
+
+        #endregion
+
+        #region caching functions
+
+        /// <summary>
+        /// add parameter array to the cache
+        /// </summary>
+        /// <param name="connectionString">a valid connection string for an OracleConnection</param>
+        /// <param name="commandText">the stored procedure name or T-OleDb command</param>
+        /// <param name="commandParameters">an array of OracleParameters to be cached</param>
+        public static void CacheParameterSet(string connectionString, string commandText, params OracleParameter[] commandParameters)
+        {
+            string hashKey = connectionString + ":" + commandText;
+            paramCache[hashKey] = commandParameters;
+        }
+        /// <summary>
+        /// retrieve a parameter array from the cache
+        /// </summary>
+        /// <param name="connectionString">a valid connection string for an OracleConnection</param>
+        /// <param name="commandText">the stored procedure name or T-OleDb command</param>
+        /// <returns>an array of OracleParameters</returns>
+        public static OracleParameter[] GetCachedParameterSet(string connectionString, string commandText)
+        {
+            string hashKey = connectionString + ":" + commandText;
+            OracleParameter[] cachedParameters = (OracleParameter[])paramCache[hashKey];
+            if (cachedParameters == null)
+            {
+                return null;
+            }
+            else
+            {
+                return CloneParameters(cachedParameters);
+            }
+        }
+
+        #endregion
+
+        #region Parameter Discovery Functions
+
+        /// <summary>
+        /// Retrieves the set of OracleParameters appropriate for the stored procedure
+        /// </summary>
+        /// <remarks>
+        /// This method will query the database for this information, and then store it in a cache for future requests.
+        /// </remarks>
+        /// <param name="connectionString">a valid connection string for an OracleConnection</param>
+        /// <param name="spName">the name of the stored procedure</param>
+        /// <returns>an array of OracleParameters</returns>
+        public static OracleParameter[] GetSpParameterSet(string connectionString, string spName)
+        {
+            return GetSpParameterSet(connectionString, spName, false);
+        }
+        /// <summary>
+        /// Retrieves the set of OracleParameters appropriate for the stored procedure
+        /// </summary>
+        /// <remarks>
+        /// This method will query the database for this information, and then store it in a cache for future requests.
+        /// </remarks>
+        /// <param name="connectionString">a valid connection string for an OracleConnection</param>
+        /// <param name="spName">the name of the stored procedure</param>
+        /// <param name="includeReturnValueParameter">a bool value indicating whether the return value parameter should be included in the results</param>
+        /// <returns>an array of OracleParameters</returns>
+        public static OracleParameter[] GetSpParameterSet(string connectionString, string spName, bool includeReturnValueParameter)
+        {
+            string hashKey = connectionString + ":" + spName + (includeReturnValueParameter ? ":include ReturnValue Parameter" : "");
+            OracleParameter[] cachedParameters;
+            cachedParameters = (OracleParameter[])paramCache[hashKey];
+            if (cachedParameters == null)
+            {
+                cachedParameters = (OracleParameter[])(paramCache[hashKey] = DiscoverSpParameterSet(connectionString, spName, includeReturnValueParameter));
+            }
+            return CloneParameters(cachedParameters);
+        }
+
+        #endregion
+
+    }
+}
