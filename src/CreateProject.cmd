3af67cef24ad77bdc012190c228b4fe1eeafabb3
CockroachSoulful <cockroach888@outlook.com>
2022-06-15 23:33:50 +0800

    feat: 增加ToolKits.Commons和ToolKits.DBUtility.TDengine两个功能库。

--- src/CreateProject.cmd
+++ src/CreateProject.cmd
@@ -23,7 +23,7 @@ dotnet new sln -n GSA2MOLLE4ToolKits
 @echo.
 @echo\&echo  ---------- 控制台示例应用程序 & DawnConsole 1 ----------
 
-dotnet new console -lang "C#" -f net5.0 -n ToolKits.DawnConsole -o samples/ToolKits.DawnConsole/src
+dotnet new console -lang "C#" -f net6.0 -n ToolKits.DawnConsole -o samples/ToolKits.DawnConsole/src
 dotnet sln GSA2MOLLE4ToolKits.sln add -s samples samples/ToolKits.DawnConsole/src
 
 
@@ -35,7 +35,7 @@ dotnet sln GSA2MOLLE4ToolKits.sln add -s samples samples/ToolKits.DawnConsole/sr
 @echo.
 @echo\&echo  ---------- 窗体示例应用程序 & DawnApp 1 ----------
 
-dotnet new winforms -lang "C#" -f net5.0 -n ToolKits.DawnApp -o samples/ToolKits.DawnApp/src
+dotnet new winforms -lang "C#" -f net6.0 -n ToolKits.DawnApp -o samples/ToolKits.DawnApp/src
 dotnet sln GSA2MOLLE4ToolKits.sln add -s samples samples/ToolKits.DawnApp/src
 
 
@@ -45,44 +45,47 @@ dotnet sln GSA2MOLLE4ToolKits.sln add -s samples samples/ToolKits.DawnApp/src
 @echo.
 @echo.
 @echo.
-@echo\&echo  ---------- 各类常用工具集 & 12 ----------
+@echo\&echo  ---------- 各类常用工具集 & 13 ----------
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.CockroachContainer -o ToolKits.CockroachContainer/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.CockroachContainer -o ToolKits.CockroachContainer/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.CockroachContainer/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.DawnUtility -o ToolKits.DawnUtility/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.DawnUtility -o ToolKits.DawnUtility/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.DawnUtility/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.FileUtility -o ToolKits.FileUtility/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.FileUtility -o ToolKits.FileUtility/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.FileUtility/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.FormUtility -o ToolKits.FormUtility/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.FormUtility -o ToolKits.FormUtility/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.FormUtility/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.GoesUtility -o ToolKits.GoesUtility/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.GoesUtility -o ToolKits.GoesUtility/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.GoesUtility/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.NuclearUtility -o ToolKits.NuclearUtility/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.NuclearUtility -o ToolKits.NuclearUtility/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.NuclearUtility/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.PagerUtility -o ToolKits.PagerUtility/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.PagerUtility -o ToolKits.PagerUtility/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.PagerUtility/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.PHYUtility -o ToolKits.PHYUtility/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.PHYUtility -o ToolKits.PHYUtility/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.PHYUtility/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.PluginsUtility -o ToolKits.PluginsUtility/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.PluginsUtility -o ToolKits.PluginsUtility/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.PluginsUtility/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.SafeUtility -o ToolKits.SafeUtility/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.SafeUtility -o ToolKits.SafeUtility/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.SafeUtility/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.VerifyUtility -o ToolKits.VerifyUtility/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.VerifyUtility -o ToolKits.VerifyUtility/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.VerifyUtility/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.WebUtility -o ToolKits.WebUtility/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.WebUtility -o ToolKits.WebUtility/src
 dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.WebUtility/src
 
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.Commons -o ToolKits.Commons/src
+dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.Commons/src
+
 
 
 
@@ -90,25 +93,28 @@ dotnet sln GSA2MOLLE4ToolKits.sln add --in-root ToolKits.WebUtility/src
 @echo.
 @echo.
 @echo.
-@echo\&echo  ---------- 数据库相关工具集 & DBUtility 6 ----------
+@echo\&echo  ---------- 数据库相关工具集 & DBUtility 7 ----------
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.DBUtility.MongoDB -o ToolKits.DBUtility/ToolKits.DBUtility.MongoDB/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.DBUtility.MongoDB -o ToolKits.DBUtility/ToolKits.DBUtility.MongoDB/src
 dotnet sln GSA2MOLLE4ToolKits.sln add -s ToolKits.DBUtility ToolKits.DBUtility/ToolKits.DBUtility.MongoDB/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.DBUtility.Mssql -o ToolKits.DBUtility/ToolKits.DBUtility.Mssql/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.DBUtility.Mssql -o ToolKits.DBUtility/ToolKits.DBUtility.Mssql/src
 dotnet sln GSA2MOLLE4ToolKits.sln add -s ToolKits.DBUtility ToolKits.DBUtility/ToolKits.DBUtility.Mssql/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.DBUtility.ODBC -o ToolKits.DBUtility/ToolKits.DBUtility.ODBC/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.DBUtility.ODBC -o ToolKits.DBUtility/ToolKits.DBUtility.ODBC/src
 dotnet sln GSA2MOLLE4ToolKits.sln add -s ToolKits.DBUtility ToolKits.DBUtility/ToolKits.DBUtility.ODBC/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.DBUtility.Oracle -o ToolKits.DBUtility/ToolKits.DBUtility.Oracle/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.DBUtility.Oracle -o ToolKits.DBUtility/ToolKits.DBUtility.Oracle/src
 dotnet sln GSA2MOLLE4ToolKits.sln add -s ToolKits.DBUtility ToolKits.DBUtility/ToolKits.DBUtility.Oracle/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.DBUtility.Sqlce -o ToolKits.DBUtility/ToolKits.DBUtility.Sqlce/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.DBUtility.Sqlce -o ToolKits.DBUtility/ToolKits.DBUtility.Sqlce/src
 dotnet sln GSA2MOLLE4ToolKits.sln add -s ToolKits.DBUtility ToolKits.DBUtility/ToolKits.DBUtility.Sqlce/src
 
-dotnet new classlib -lang "C#" -f net5.0 -n ToolKits.DBUtility.SQLite -o ToolKits.DBUtility/ToolKits.DBUtility.SQLite/src
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.DBUtility.SQLite -o ToolKits.DBUtility/ToolKits.DBUtility.SQLite/src
 dotnet sln GSA2MOLLE4ToolKits.sln add -s ToolKits.DBUtility ToolKits.DBUtility/ToolKits.DBUtility.SQLite/src
+
+dotnet new classlib -lang "C#" -f net6.0 -n ToolKits.DBUtility.TDengine -o ToolKits.DBUtility/ToolKits.DBUtility.TDengine/src
+dotnet sln GSA2MOLLE4ToolKits.sln add -s ToolKits.DBUtility ToolKits.DBUtility/ToolKits.DBUtility.TDengine/src
 
 
 
