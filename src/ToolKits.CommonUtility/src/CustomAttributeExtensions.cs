76150ab93036b5080e597277dcbdc6e11526a447
CockroachSoulful <cockroach888@outlook.com>
2022-09-01 23:04:52 +0800

    feat: 增加自定义属性助手类和扩展类。
    PS：用于判断是否使用某自定义属性，及获取自定义属性成员元数据。

---
+++ src/ToolKits.CommonUtility/src/CustomAttributeExtensions.cs
@@ -0,0 +1,53 @@
+﻿//=========================================================================
+//**   魂哥常用工具集（CRS.ToolKits）
+//=========================================================================
+//**   脉脉含情的充满精神的高尚的小强精神
+//**   风幽思静繁花落；夜半楼台听江雨。（cockroach888@outlook.com）
+//=========================================================================
+//**   Copyright © 蟑螂·魂 2022 -- Support 华夏银河空间联盟
+//=========================================================================
+// 文件名称：CustomAttributeExtensions.cs
+// 项目名称：魂哥常用工具集
+// 创建时间：2022-09-01 14:36:01
+// 创建人员：宋杰军
+// 电子邮件：cockroach888@outlook.com
+// 负责人员：宋杰军
+// 参与人员：宋杰军
+// ========================================================================
+// 修改日期：
+// 修改人员：
+// 修改内容：
+// ========================================================================
+using GSA.ToolKits.CommonUtility;
+using System.Reflection;
+
+/// <summary>
+/// 自定义属性扩展类
+/// </summary>
+public static class CustomAttributeExtensions
+{
+    /// <summary>
+    /// 获取自定义属性中指定成员元数据的值
+    /// </summary>
+    /// <typeparam name="TAttribute">自定义属性泛型</typeparam>
+    /// <typeparam name="TValue">数据类型泛型</typeparam>
+    /// <param name="member">表示成员的属性信息</param>
+    /// <param name="valueCallback">用于获取成员元数据的回调函数</param>
+    /// <returns>成员元数据的值</returns>
+    public static TValue? GetCustomAttributeValue<TAttribute, TValue>(this MemberInfo member, Func<TAttribute, TValue> valueCallback)
+        where TAttribute : Attribute
+        => CustomAttributeHelper.GetValue(member, valueCallback);
+
+    /// <summary>
+    /// 获取自定义属性中指定成员元数据的值
+    /// </summary>
+    /// <typeparam name="TAttribute">自定义属性泛型</typeparam>
+    /// <typeparam name="TValue">数据类型泛型</typeparam>
+    /// <param name="type">应用自定义属性的类型对象</param>
+    /// <param name="valueCallback">用于获取成员元数据的回调函数</param>
+    /// <param name="memberName">成员元数据的名称</param>
+    /// <returns>成员元数据的值</returns>
+    public static TValue? GetCustomAttributeValue<TAttribute, TValue>(this Type type, Func<TAttribute, TValue> valueCallback, string? memberName = null)
+        where TAttribute : Attribute
+        => CustomAttributeHelper.GetValue(type, valueCallback, memberName);
+}
---
+++ src/ToolKits.CommonUtility/src/CustomAttributeHelper.cs
@@ -0,0 +1,138 @@
+﻿//=========================================================================
+//**   魂哥常用工具集（CRS.ToolKits）
+//=========================================================================
+//**   脉脉含情的充满精神的高尚的小强精神
+//**   风幽思静繁花落；夜半楼台听江雨。（cockroach888@outlook.com）
+//=========================================================================
+//**   Copyright © 蟑螂·魂 2022 -- Support 华夏银河空间联盟
+//=========================================================================
+// 文件名称：CustomAttributeValueHelper.cs
+// 项目名称：魂哥常用工具集
+// 创建时间：2022-09-01 13:48:12
+// 创建人员：宋杰军
+// 电子邮件：cockroach888@outlook.com
+// 负责人员：宋杰军
+// 参与人员：宋杰军
+// ========================================================================
+// 修改日期：
+// 修改人员：
+// 修改内容：
+// ========================================================================
+using System.Reflection;
+
+namespace GSA.ToolKits.CommonUtility;
+
+/// <summary>
+/// 自定义属性助手类
+/// </summary>
+public static class CustomAttributeHelper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TAttribute"></typeparam>
+    /// <param name="member"></param>
+    /// <returns></returns>
+    public static bool Exists<TAttribute>(MemberInfo? member)
+        where TAttribute : Attribute
+    {
+        if (member is null)
+        {
+            return false;
+        }
+
+        return member.GetCustomAttributes<TAttribute>(false).Any();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TAttribute"></typeparam>
+    /// <param name="type"></param>
+    /// <param name="memberName"></param>
+    /// <returns></returns>
+    public static bool Exists<TAttribute>(Type type, string? memberName = null)
+        where TAttribute : Attribute
+    {
+        if (memberName is null)
+        {
+            return type.GetCustomAttributes<TAttribute>(false).Any();
+        }
+
+        MemberInfo? member = type.GetMember(memberName).FirstOrDefault();
+        return Exists<TAttribute>(member);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TType"></typeparam>
+    /// <typeparam name="TAttribute"></typeparam>
+    /// <param name="memberName"></param>
+    /// <returns></returns>
+    public static bool Exists<TType, TAttribute>(string? memberName = null)
+        where TType : class
+        where TAttribute : Attribute
+        => Exists<TAttribute>(typeof(TType), memberName);
+
+    /// <summary>
+    /// 获取自定义属性中指定成员元数据的值
+    /// </summary>
+    /// <typeparam name="TAttribute">自定义属性泛型</typeparam>
+    /// <typeparam name="TValue">数据类型泛型</typeparam>
+    /// <param name="member">表示成员的属性信息</param>
+    /// <param name="valueCallback">用于获取成员元数据的回调函数</param>
+    /// <returns>成员元数据的值</returns>
+    public static TValue? GetValue<TAttribute, TValue>(MemberInfo? member, Func<TAttribute, TValue> valueCallback)
+        where TAttribute : Attribute
+    {
+        if (member is null)
+        {
+            return default;
+        }
+
+        TAttribute? attr = member.GetCustomAttributes<TAttribute>(false).FirstOrDefault();
+        return attr is null ? default : valueCallback(attr);
+    }
+
+    /// <summary>
+    /// 获取自定义属性中指定成员元数据的值
+    /// </summary>
+    /// <typeparam name="TAttribute">自定义属性泛型</typeparam>
+    /// <typeparam name="TValue">数据类型泛型</typeparam>
+    /// <param name="type">应用自定义属性的类型对象</param>
+    /// <param name="valueCallback">用于获取成员元数据的回调函数</param>
+    /// <param name="memberName">成员元数据的名称</param>
+    /// <returns>成员元数据的值</returns>
+    public static TValue? GetValue<TAttribute, TValue>(Type type, Func<TAttribute, TValue> valueCallback, string? memberName = null)
+        where TAttribute : Attribute
+    {
+        TAttribute? attr;
+
+        if (memberName is null)
+        {
+            attr = type.GetCustomAttributes<TAttribute>(false).FirstOrDefault();
+        }
+        else
+        {
+            MemberInfo? member = type.GetMember(memberName).FirstOrDefault();
+            attr = member?.GetCustomAttributes<TAttribute>(false).FirstOrDefault();
+        }
+
+        return attr is null ? default : valueCallback(attr);
+    }
+
+    /// <summary>
+    /// 获取自定义属性中指定成员元数据的值
+    /// </summary>
+    /// <typeparam name="TType">应用自定义属性的泛型</typeparam>
+    /// <typeparam name="TAttribute">自定义属性泛型</typeparam>
+    /// <typeparam name="TValue">数据类型泛型</typeparam>
+    /// <param name="valueCallback">用于获取成员元数据的回调函数</param>
+    /// <param name="memberName">成员元数据的名称</param>
+    /// <returns>成员元数据的值</returns>
+    public static TValue? GetValue<TType, TAttribute, TValue>(Func<TAttribute, TValue> valueCallback, string? memberName = null)
+        where TType : class
+        where TAttribute : Attribute
+        => GetValue(typeof(TType), valueCallback, memberName);
+}
