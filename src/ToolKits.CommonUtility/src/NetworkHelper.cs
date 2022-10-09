27d0d5e09984fe91634cc012c50760435754807c
CockroachSoulful <cockroach888@outlook.com>
2022-10-09 12:54:35 +0800

    feat: 增加网络与通信助手类(NetworkHelper)。
    1.增加物理IP地址和MAC地址获取方法；
    2.增加应用测试示例，并验证功能。

---
+++ src/ToolKits.CommonUtility/src/NetworkHelper.cs
@@ -0,0 +1,130 @@
+﻿//=========================================================================
+//**   魂哥常用工具集（CRS.ToolKits）
+//=========================================================================
+//**   脉脉含情的充满精神的高尚的小强精神
+//**   风幽思静繁花落；夜半楼台听江雨。（cockroach888@outlook.com）
+//=========================================================================
+//**   Copyright © 蟑螂·魂 2022 -- Support 华夏银河空间联盟
+//=========================================================================
+// 文件名称：NetworkHelper.cs
+// 项目名称：魂哥常用工具集
+// 创建时间：2022-10-09 12:22:19
+// 创建人员：宋杰军
+// 电子邮件：cockroach888@outlook.com
+// 负责人员：宋杰军
+// 参与人员：宋杰军
+// ========================================================================
+// 修改日期：
+// 修改人员：
+// 修改内容：
+// ========================================================================
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Net;
+
+namespace GSA.ToolKits.CommonUtility;
+
+/// <summary>
+/// 网络与通信助手类
+/// </summary>
+public static class NetworkHelper
+{
+    /// <summary>
+    /// 获取真实有效的本地IP地址
+    /// </summary>
+    /// <returns>本地IP地址</returns>
+    public static string GetIpAddress()
+    {
+        try
+        {
+            UnicastIPAddressInformation? mostSuitableIp = null;
+            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface network in networkInterfaces)
+            {
+                if (network.OperationalStatus is not OperationalStatus.Up)
+                {
+                    continue;
+                }
+
+                IPInterfaceProperties properties = network.GetIPProperties();
+
+                if (properties.GatewayAddresses.Count is 0)
+                {
+                    continue;
+                }
+
+                foreach (UnicastIPAddressInformation address in properties.UnicastAddresses)
+                {
+                    if (address.Address.AddressFamily is not AddressFamily.InterNetwork)
+                    {
+                        continue;
+                    }
+
+                    if (IPAddress.IsLoopback(address.Address))
+                    {
+                        continue;
+                    }
+
+                    if (address.IsDnsEligible is false)
+                    {
+                        mostSuitableIp ??= address;
+                        continue;
+                    }
+
+                    // The best IP is the IP got from DHCP server
+                    if (address.PrefixOrigin is not PrefixOrigin.Dhcp)
+                    {
+                        if (mostSuitableIp is null || mostSuitableIp.IsDnsEligible is false)
+                        {
+                            mostSuitableIp = address;
+                        }
+
+                        continue;
+                    }
+
+                    return address.Address.ToString();
+                }
+            }
+
+            return mostSuitableIp is not null ? mostSuitableIp.Address.ToString() : "";
+        }
+        catch { }
+
+        return "0.0.0.0";
+    }
+
+    /// <summary>
+    /// 获得指定IP地址对应的MAC地址
+    /// </summary>
+    /// <remarks>MAC地址将以000000000000或00-00-00-00-00-00的形式返回，默认值全为零。</remarks>
+    /// <param name="ipAddress">IP地址</param>
+    /// <param name="isUseHorizontalBars">是否使用橫杠分隔</param>
+    /// <returns>MAC地址</returns>
+    public static string GetMacAddress(string ipAddress, bool isUseHorizontalBars = false)
+    {
+        try
+        {
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (var face in interfaces)
+            {
+                UnicastIPAddressInformationCollection unicast = face.GetIPProperties().UnicastAddresses;
+
+                foreach (UnicastIPAddressInformation cast in unicast)
+                {
+                    string value = cast.Address.ToString();
+
+                    if (ipAddress.Equals(value))
+                    {
+                        string mac = BitConverter.ToString(face.GetPhysicalAddress().GetAddressBytes());
+                        return isUseHorizontalBars ? mac : mac.Replace("-", "");
+                    }
+                }
+            }
+        }
+        catch (Exception) { }
+
+        return isUseHorizontalBars ? "00-00-00-00-00-00" : "000000000000";
+    }
+}
--- src/samples/ToolKits.DawnConsole/src/Program.cs
+++ src/samples/ToolKits.DawnConsole/src/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GSA.ToolKits.CommonUtility;
+using System;
 
 namespace GSA.ToolKits.DawnConsole
 {
@@ -7,6 +8,20 @@ namespace GSA.ToolKits.DawnConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string ip = NetworkHelper.GetIpAddress();
+            string mac1 = NetworkHelper.GetMacAddress(ip);
+            string mac2 = NetworkHelper.GetMacAddress(ip, isUseHorizontalBars: true);
+
+            Console.WriteLine($"IP：{ip}  MAC：{mac1}  {mac2}");
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("按任意键继续...");
+            Console.ReadKey();
         }
     }
 }
