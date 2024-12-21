add917c09f829c3ef7cc7c78379ebe9e7690ed16
CockroachSoulful <cockroach888@outlook.com>
2024-12-22 00:41:56 +0800

    feat: 完成TDengine使用WinForm直接使用示例的编码这实现。

---
+++ src/samples/TaosData/TDengineEx.Appx4WinForm/src/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TDengineEx.Appx4WinForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnConnection = new Button();
+            txtMessage = new TextBox();
+            btnQueryLog = new Button();
+            SuspendLayout();
+            // 
+            // btnConnection
+            // 
+            btnConnection.Location = new Point(12, 17);
+            btnConnection.Name = "btnConnection";
+            btnConnection.Size = new Size(227, 32);
+            btnConnection.TabIndex = 0;
+            btnConnection.Text = "使用独立连接显示数据库信息";
+            btnConnection.UseVisualStyleBackColor = true;
+            btnConnection.Click += BtnConnection_Click;
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(12, 67);
+            txtMessage.Multiline = true;
+            txtMessage.Name = "txtMessage";
+            txtMessage.ScrollBars = ScrollBars.Vertical;
+            txtMessage.Size = new Size(760, 482);
+            txtMessage.TabIndex = 1;
+            // 
+            // btnQueryLog
+            // 
+            btnQueryLog.Location = new Point(261, 17);
+            btnQueryLog.Name = "btnQueryLog";
+            btnQueryLog.Size = new Size(221, 32);
+            btnQueryLog.TabIndex = 0;
+            btnQueryLog.Text = "使用全局连接查询日志数据";
+            btnQueryLog.UseVisualStyleBackColor = true;
+            btnQueryLog.Click += BtnQueryLog_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 561);
+            Controls.Add(txtMessage);
+            Controls.Add(btnQueryLog);
+            Controls.Add(btnConnection);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MainForm";
+            FormClosed += MainForm_FormClosed;
+            Load += MainForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnConnection;
+        private TextBox txtMessage;
+        private Button btnQueryLog;
+    }
+}
---
+++ src/samples/TaosData/TDengineEx.Appx4WinForm/src/MainForm.cs
@@ -0,0 +1,97 @@
+﻿using GSA.ToolKits.DBUtility.TDengine;
+
+namespace TDengineEx.Appx4WinForm;
+
+public partial class MainForm : Form
+{
+    private ITDengineConnector? _connector;
+
+
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+        TDengineOptions options = new()
+        {
+            Host = "127.0.0.1",
+            Port = 10101,
+            UserName = "root",
+            Password = "taosdata",
+            TimeZone = "Asia/Shanghai",
+            VersionSelector = TDengineVersion.V3,
+            KeyNameRegex = [
+                "last_row\\((.+?)\\)",
+                "last\\((.+?)\\)",
+                "first\\((.+?)\\)",
+                "sum\\((.+?)\\)"
+            ]
+        };
+
+        _connector = TDengineConnectorProvider.Default.Create(options);
+    }
+
+    private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+    {
+        if (_connector is not null)
+        {
+            using (_connector) { }
+        }
+    }
+
+
+    private async void BtnConnection_Click(object sender, EventArgs e)
+    {
+        TDengineOptions options = new()
+        {
+            Host = "127.0.0.1",
+            Port = 10101,
+            UserName = "root",
+            Password = "taosdata",
+            TimeZone = "Asia/Shanghai",
+            VersionSelector = TDengineVersion.V3,
+            KeyNameRegex = [
+                "last_row\\((.+?)\\)",
+                "last\\((.+?)\\)",
+                "first\\((.+?)\\)",
+                "sum\\((.+?)\\)"
+            ]
+        };
+
+        using ITDengineConnector connector = TDengineConnectorProvider.Default.Create(options);
+
+        TDengineQueryParam param = new("show databases;");
+        string? result = await connector.ExecuteAsync(param).ConfigureAwait(false);
+
+        if (string.IsNullOrWhiteSpace(result) is false)
+        {
+            BeginInvoke(() =>
+            {
+                txtMessage.Text = result;
+            });
+        }
+    }
+
+    private async void BtnQueryLog_Click(object sender, EventArgs e)
+    {
+        if (_connector is null)
+        {
+            txtMessage.Text = "还未创建 TDengine RESTful API 连接器，请确认后重试。";
+            return;
+        }
+
+        TDengineQueryParam param = new("select * from log.keeper_monitor;");
+        string? result = await _connector.ExecuteAsync(param).ConfigureAwait(false);
+
+        if (string.IsNullOrWhiteSpace(result) is false)
+        {
+            BeginInvoke(() =>
+            {
+                txtMessage.Text = result;
+            });
+        }
+    }
+}
