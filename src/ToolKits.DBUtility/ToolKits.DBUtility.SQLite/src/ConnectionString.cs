0ed8e3eb89b1b626de49388e9dfbcc034fbd8000
CockroachSoulful <cockraoch888@outlook.com>
2026-03-19 16:27:51 +0800

    feat: 实践.NET10系列技术体系。

--- src/ToolKits.DBUtility/ToolKits.DBUtility.SQLite/src/ConnectionString.cs
+++ src/ToolKits.DBUtility/ToolKits.DBUtility.SQLite/src/ConnectionString.cs
@@ -19,49 +19,48 @@
 // ========================================================================
 using System.Text;
 
-namespace GSA.ToolKits.DBUtility
+namespace GSA.ToolKits.DBUtility;
+
+/// <summary>
+/// 数据库连接字符串
+/// </summary>
+public sealed class SQliteConnectionString
 {
     /// <summary>
-    /// 数据库连接字符串
+    /// SQLite数据库连接字符串
+    /// <para>调用时请对此方法特别调用</para>
     /// </summary>
-    public sealed class SQliteConnectionString
+    /// <param name="dataPath">SQLite数据库文件路径</param>
+    /// <param name="pwd">数据库密码</param>
+    /// <param name="isShared">是否共享缓存</param>
+    /// <param name="isReadOnly">是否只读打开</param>
+    /// <returns>SQLite数据库连接字符串</returns>
+    public static string ConnectionString(string dataPath, string pwd, bool isShared = true, bool isReadOnly = false)
     {
-        /// <summary>
-        /// SQLite数据库连接字符串
-		/// <para>调用时请对此方法特别调用</para>
-        /// </summary>
-        /// <param name="dataPath">SQLite数据库文件路径</param>
-        /// <param name="pwd">数据库密码</param>
-        /// <param name="isShared">是否共享缓存</param>
-        /// <param name="isReadOnly">是否只读打开</param>
-        /// <returns>SQLite数据库连接字符串</returns>
-		public static string ConnectionString(string dataPath, string pwd, bool isShared = true, bool isReadOnly = false)
+        var sb = new StringBuilder();
+
+        sb.Append($@"Data Source={dataPath};");
+        sb.Append($@"Pooling=true;");
+        sb.Append($@"FailIfMissing=false;");
+
+        // 共享缓存提高并发
+        if (isShared)
         {
-            var sb = new StringBuilder();
-
-            sb.Append($@"Data Source={dataPath};");
-            sb.Append($@"Pooling=true;");
-            sb.Append($@"FailIfMissing=false;");
-
-            // 共享缓存提高并发
-            if (isShared)
-            {
-                sb.Append($@"Cache=Shared;");
-            }
-
-            // 加密
-            if (!string.IsNullOrEmpty(pwd))
-            {
-                sb.Append($@"Password={pwd};");
-            }
-
-            // 只读
-            if (isReadOnly)
-            {
-                sb.Append($@"Mode=ReadOnly;");
-            }
-
-            return sb.ToString();
+            sb.Append($@"Cache=Shared;");
         }
+
+        // 加密
+        if (!string.IsNullOrEmpty(pwd))
+        {
+            sb.Append($@"Password={pwd};");
+        }
+
+        // 只读
+        if (isReadOnly)
+        {
+            sb.Append($@"Mode=ReadOnly;");
+        }
+
+        return sb.ToString();
     }
 }
